6deea1346c459e85e4fe445611bd34002659e6b4
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-03-06 01:04:32 +0900

    MySQL로 상세보기 구현(주석 추가)

--- main.js
+++ main.js
@@ -56,11 +56,11 @@ var app = http.createServer(function(request, response) {
                   });
               });
               */
-            db.query(`SELECT * FROM topic`, function(error, topics) {
-                if (error) {
-                    throw error; //error를 던저벼린다.(즉시 멈춤)
+            db.query(`SELECT * FROM topic`, function(error, topics) { //topic값 가져옴
+                if (error) { //topic을 못가져온다면 (에러값 생성)
+                    throw error; //error를 던저벼린다.(그다음 코드 실행하지 않고 console에 오류 표시하며 즉시 멈춤)
                 }
-                db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic) {
+                db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic) { //?에 들어갈 값을 배열에 인자로 넣어줌.->사용자가 입력한 정보는 무조건 불신해야하므로 ${}대신 저렇게 씀. []가 그런걸 해결해줌.
                     if (error2) {
                         throw error2;
                     }
