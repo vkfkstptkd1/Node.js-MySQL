2288bc5aed0d706fbb2255622dcad65afd54229c
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-03-24 15:31:43 +0900

    Node.js 코드의 정리정돈 - page, create, create_process

--- main.js
+++ main.js
@@ -13,80 +13,12 @@ var app = http.createServer(function(request, response) {
         if (queryData.id === undefined) {
             topic.home(request, response);
         } else { //글 목록 불러오는 코드
-            db.query(`SELECT * FROM topic`, function(error, topics) { //topic값 가져옴
-                if (error) { //topic을 못가져온다면 (에러값 생성)
-                    throw error; //error를 던저벼린다.(그다음 코드 실행하지 않고 console에 오류 표시하며 즉시 멈춤)
-                }
-                db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [queryData.id], function(error2, topic) { //?에 들어갈 값을 배열에 인자로 넣어줌.->사용자가 입력한 정보는 무조건 불신해야하므로 ${}대신 저렇게 씀. []가 그런걸 해결해줌.
-                    //글 상세보기
-                    if (error2) {
-                        throw error2;
-                    }
-                    console.log(topic[0].title);
-                    var title = topic[0].title;
-                    var description = topic[0].description;
-                    var list = template.list(topics);
-                    var html = template.HTML(title, list,
-                        `<h2>${title}</h2>${description}
-                        <p>by ${topic[0].name}</p>`, //누가썼는지 본문에 구현.
-                        `<a href="/create">create</a>
-                         <a href="/update?id=${queryData.id}">update</a>
-                         <form action="delete_process" method="post">
-                           <input type="hidden" name="id" value="${queryData.id}">
-                           <input type="submit" value="delete">
-                         </form>`
-                    );
-                    response.writeHead(200);
-                    response.end(html);
-                })
-            });
-
+            topic.page(request, response);
         }
     } else if (pathname === '/create') {
-        db.query(`SELECT * FROM topic`, function(error, topics) {
-            db.query(`SELECT * FROM author`, function(error2, authors) {
-
-                var title = 'Create';
-                var list = template.list(topics);
-                var html = template.HTML(title, list,
-                    ` <form action="/create_process" method="post">
-                    <p><input type="text" name="title" placeholder="title"></p>
-                    <p>
-                     <textarea name="description" placeholder="description"></textarea>
-                    </p>
-                    <p>
-                    ${template.authorSelect(authors)}
-                    </p>
-                    <p>
-                      <input type="submit">
-                    </p>
-                    </form>`,
-                    `<a href="/create">create</a>`
-                );
-                response.writeHead(200);
-                response.end(html);
-            });
-        });
-
+        topic.create(request, response);
     } else if (pathname === '/create_process') { //사용자가 입력한 글생성
-        var body = '';
-        request.on('data', function(data) {
-            body = body + data;
-        });
-        request.on('end', function() {
-            var post = qs.parse(body);
-
-            db.query(`
-                INSERT INTO topic (title, description, created, author_id) 
-                 VALUES(?,?, NOW(),?);`, [post.title, post.description, post.author],
-                function(error, result) {
-                    if (error) {
-                        throw error;
-                    }
-                    response.writeHead(302, { Location: `/?id=${result.insertId}` }); //삽입한 행의 id값= .insertId
-                    response.end();
-                })
-        });
+        topic.create_process(request, response);
     } else if (pathname === '/update') { //글수정
         db.query('SELECT * FROM topic', function(error, topics) {
             if (error) {
