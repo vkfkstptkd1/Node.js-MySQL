fb3db45e3ab7243d9d52f76d5366f3ba899788e7
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-03-30 18:50:04 +0900

    저자 목록 보기 기능 구현

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var db = require('./lib/db');
 var topic = require('./lib/topic');
+var author = require('./lib/author');
+const { authorSelect } = require('./lib/template.js');
 
 var app = http.createServer(function(request, response) {
     var _url = request.url;
@@ -25,6 +27,8 @@ var app = http.createServer(function(request, response) {
         topic.update_process(request, response);
     } else if (pathname === '/delete_process') { //글 삭제
         topic.delete_process(request, response);
+    } else if (pathname === '/author') {
+        author.home(request, response);
     } else {
         response.writeHead(404);
         response.end('Not found');
